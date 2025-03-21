78146b53165eac90ecdaab5d3c87688e1a0328fe
Yevhenii Pashko <y.pashcode@gmail.com>
2025-03-21 13:42:19 +0200

    feat: add vite-plugin-html for HTML minification

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
+  plugins: [
+    createHtmlPlugin({
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+      },
+    }),
+  ],
   base: "/calm-pairs/",
   server: {
     open: true,
