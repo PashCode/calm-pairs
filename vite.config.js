9e532a9d74f00d62d947f2ffaaa25e92e23deab0
Yevhenii Pashko <y.pashcode@gmail.com>
2025-03-21 15:02:09 +0200

    chore: edit vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
+  base: "/calm-pairs/",
+  server: {
+    open: true,
+    port: 3001,
+  },
   plugins: [
     createHtmlPlugin({
       minify: {
@@ -10,9 +15,4 @@ export default defineConfig({
       },
     }),
   ],
-  base: "/calm-pairs/",
-  server: {
-    open: true,
-    port: 3001,
-  },
 });
